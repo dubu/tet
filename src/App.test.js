d38e8925f76c4d0f5c3aea64fb4cbb525c5c1dc6
rigel <rigel.k@kakaocorp.com>
2018-01-31 18:59:48 +0900

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
-it('renders without crashing', () => {
+it('renders App', () => {
     console.log("test")
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('collision', function () {
+
+    console.log("coll")
+    
+}); 
